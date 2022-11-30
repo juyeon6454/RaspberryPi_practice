7815319d8b57d953908785cb1ea6fa098a0a1888
juyeon6454 <juyeon6454@gmail.com>
2022-11-30 14:40:36 +0900

    221130 14:40

--- add2.c
+++ add2.c
@@ -1,17 +1,11 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h>
-#include "bmpHeader.h"
+
 #define BYTE unsigned char
 
 #define widthbytes(bits) (((bits)+31)/32*4)
-
-typedef struct tagRGBQUAD{
-	BYTE rgbBlue; 
-	BYTE rgbGreen; 
-	BYTE rgbRed; 
-	BYTE rgbReserved;
-} RGBQUAD;
+#include "bmpHeader.h"
 
 inline unsigned char clip(int value, int min, int max);
 unsigned char clip(int value, int min, int max)
@@ -23,13 +17,11 @@ int main(int argc, char** argv) {
 	FILE* fp; 
 	RGBQUAD palrgb[256];
 	
+	BITMAPFILEHEADER bmpFileHeader;
+	BITMAPINFOHEADER bmpInfoHeader;
+
 	/* BMP FILE INFO */
-	/* BMP IMAGE INFO */
-
-BITMAPFILEHEADER bmpHeader;
-BITMAPINFOHEADER bmpInfoHeader;
-
-	char input[128], output[128];
+		char input[128], output[128];
 	
 	int i, j, size; 
 	float srcX, srcY;
@@ -39,7 +31,7 @@ BITMAPINFOHEADER bmpInfoHeader;
 	int index2;
 	
 	unsigned char *grayimg, *inimg, *outimg;
-	
+	unsigned imagesize;	
 	/* usage a.out in.bmp out.bmp */
 	strcpy(input, argv[1]); 
 	strcpy(output, argv[2]);
@@ -49,14 +41,15 @@ BITMAPINFOHEADER bmpInfoHeader;
 		fprintf(stderr, "Error : Failed to open file...₩n"); 
 		return -1;
 	}
+	fread(&bmpFileHeader, sizeof(bmpFileHeader),1,fp);	
+	fread(&bmpInfoHeader, sizeof(bmpInfoHeader),1,fp);
 
-	fread(&bmpHeader, sizeof(BITMAPFILEHEADER), 1, fp);
-	fread(&bmpInfoHeader, sizeof(BITMAPINFOHEADER, 1, fp);
-	size=widthbytes(bits * width); 
-	graysize = widthbytes(8 * width);
+
+	size=widthbytes(bmpInfoHeader.biBitCount * bmpInfoHeader.biWidth); 
+	graysize = widthbytes(8 * bmpInfoHeader.biWidth);
 	
 	if(!imagesize) 
-		imagesize=height*size;
+		imagesize=bmpInfoHeader.biHeight*size;
 	
 	inimg=(BYTE*)malloc(sizeof(BYTE)*imagesize); 
 	outimg=(BYTE*)malloc(sizeof(BYTE)*imagesize); 
@@ -64,48 +57,33 @@ BITMAPINFOHEADER bmpInfoHeader;
 	
 	fclose(fp);
 	
-	for(i=0; i<height*3; i+=3) { 
-		for(j=0; j<width*3; j+=3) {
-			int b = inimg[j+(i*width+0)]; 
-			int g = inimg[j+(i*width+1)]; 
-			int r = inimg[j+(i*width+2)]; 
+	for(i=0; i<bmpInfoHeader.biHeight*3; i+=3) { 
+		for(j=0; j<bmpInfoHeader.biWidth*3; j+=3) {
+			int b = inimg[j+(i*bmpInfoHeader.biWidth+0)]; 
+			int g = inimg[j+(i*bmpInfoHeader.biWidth+1)]; 
+			int r = inimg[j+(i*bmpInfoHeader.biWidth+2)]; 
 			
 			b += 50;
 			g += 50;
 			r += 50; 
 			
-			outimg[j+width*i+0]= clip(b, 0, 255);
-			outimg[j+width*i+1]= clip(g, 0, 255);
-			outimg[j+width*i+2]= clip(r, 0, 255);
+			outimg[j+bmpInfoHeader.biWidth*i+0]= clip(b, 0, 255);
+			outimg[j+bmpInfoHeader.biWidth*i+1]= clip(g, 0, 255);
+			outimg[j+bmpInfoHeader.biWidth*i+2]= clip(r, 0, 255);
 		};
 	 };	   
 	 
-	size=widthbytes(bits*width); 
-	imagesize=height*size; 
-	offset+=256*sizeof(RGBQUAD);
+	
+	size=widthbytes(bmpInfoHeader.biBitCount*bmpInfoHeader.biWidth); 
+	imagesize=bmpInfoHeader.biHeight*size; 
+	bmpFileHeader.bfOffBits+=256*sizeof(RGBQUAD);
 	
 	if((fp=fopen(output, "wb"))==NULL) { 
 		fprintf(stderr, "Error : Failed to open file...₩n"); 
 		return -1;
 	}
-	
-	fwrite(&type, sizeof(unsigned short int), 1, fp); 
-	fwrite(&file_size, sizeof(unsigned int), 1, fp); 
-	fwrite(&reserved1, sizeof(unsigned short int), 1, fp); 
-	fwrite(&reserved2, sizeof(unsigned short int), 1, fp); 
-	fwrite(&offset, sizeof(unsigned int), 1, fp); 
-	
-	fwrite(&header_size, sizeof(unsigned int), 1, fp); 
-	fwrite(&width, sizeof(int), 1, fp);
-	fwrite(&height, sizeof(int), 1, fp);
-	fwrite(&planes, sizeof(unsigned short int), 1, fp); 
-	fwrite(&bits, sizeof(unsigned short int), 1, fp);		
-	fwrite(&compression, sizeof(unsigned int), 1, fp); 
-	fwrite(&imagesize, sizeof(unsigned int), 1, fp); 
-	fwrite(&hresolution, sizeof(int), 1, fp); 
-	fwrite(&vresolution, sizeof(int), 1, fp); 
-	fwrite(&ncolors, sizeof(unsigned int), 1, fp); 
-	fwrite(&importantcolors, sizeof(unsigned int), 1, fp);
+	fwrite(&bmpFileHeader, sizeof(bmpFileHeader),1,fp);
+	fwrite(&bmpInfoHeader, sizeof(bmpInfoHeader),1,fp);
 	fwrite(palrgb, sizeof(unsigned int), 256, fp); 
 	
 	fwrite(outimg, sizeof(unsigned char), imagesize, fp);
@@ -117,4 +95,3 @@ BITMAPINFOHEADER bmpInfoHeader;
 	
 	return 0;
 }
-
