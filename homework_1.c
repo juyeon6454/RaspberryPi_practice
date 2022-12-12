fe296308779c91ece1581486a6aa46de636b8369
juyeon6454 <juyeon6454@gmail.com>
2022-12-12 23:20:15 +0900

    221212 23:20

---
+++ homework_1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bmpHeader.h"
+
+unsigned char UBYTE;		
+
+int main(int argc, char **argv)
+{
+		FILE *fp;
+		BITMAPFILEHEADER bmpFileHeader;
+		BITMAPINFOHEADER bmpInfoHeader;
+		unsigned char *inimg, *outimg;
+		char input[100], output[100];
+		int imagesize;
+		strcpy(input, argv[1]);
+		strcpy(output, argv[2]);
+
+		if((fp = fopen(argv[1],"rb")) ==NULL){
+			fprintf(stderr, "Error : Failed to open file...\n");
+			exit(EXIT_FAILURE);
+		}
+
+		fread(&bmpFileHeader, sizeof(bmpFileHeader),1,fp);
+		fread(&bmpInfoHeader, sizeof(bmpInfoHeader),1,fp);
+		printf("width : %d height : %d", bmpInfoHeader.biWidth , bmpInfoHeader.biHeight);	
+		//if(!imagesize) imagesize = bmpInfoHeader.biHeight * size;
+		int elemsize = bmpInfoHeader.biBitCount/8;
+		imagesize = bmpInfoHeader.biHeight * bmpInfoHeader.biWidth * elemsize;
+		printf("imagesize : %d " , imagesize);
+
+		inimg =malloc(sizeof(UBYTE)*imagesize);
+		outimg =malloc(sizeof(UBYTE)*imagesize);
+
+		fread(inimg, sizeof(UBYTE),  imagesize, fp);
+		fread(outimg, sizeof(UBYTE), imagesize, fp);
+		fclose(fp);
+	
+		for(int i = 0; i < bmpInfoHeader.biHeight; i++) {
+				for(int j=0; j<bmpInfoHeader.biWidth * elemsize; j+=elemsize) 
+				{
+							outimg[j+(i*bmpInfoHeader.biWidth*elemsize+0)]=inimg[j+(i*bmpInfoHeader.biWidth * elemsize + 0)];
+					
+							outimg[j+(i*bmpInfoHeader.biWidth*elemsize+1)]=inimg[j+(i*bmpInfoHeader.biWidth * elemsize + 1)];	
+							
+							outimg[j+(i*bmpInfoHeader.biWidth*elemsize+2)]=inimg[j+(i*bmpInfoHeader.biWidth * elemsize + 2)];
+				}
+		}
+		if((fp = fopen(argv[2], "w")) == NULL){
+				fprintf(stderr, "Error : Failed to open file...\n");
+				exit(EXIT_FAILURE);
+		}
+		fwrite(&bmpFileHeader, sizeof(bmpFileHeader), 1, fp);
+		fwrite(&bmpInfoHeader, sizeof(bmpInfoHeader), 1, fp);
+
+		fwrite(outimg, sizeof(UBYTE), 1, fp);
+		free(inimg);
+		free(outimg);
+	    fclose(fp);
+		return 0;
+}
